ca84ff97ef26d74baf64b3a2f43018bd2d208037
vmkul <55065764+vmkul@users.noreply.github.com>
2020-05-22 19:19:10 +0300

    New command

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+const { prefix } = require('../config.json');
+
+module.exports = {
+  name: 'help',
+  description: 'List a description of available commands',
+  execute(message, args, client) {
+    let result = '```';
+    for (const command of client.commands) {
+      result += `${prefix}${command[1].name} - ${command[1].description}\n`;
+    }
+    message.channel.send(result + '```');
+  },
+};
